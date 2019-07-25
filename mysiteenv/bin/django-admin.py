fdafbfdfb4cce09118c02d52d519e98eb71d88d0
itsaiub <itsaiub@gmail.com>
2019-07-26 01:28:22 +0600

    project initialized

---
+++ mysiteenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/a1ubkh4n/Documents/Developmnet/My_Django_Stuff/mysite/mysiteenv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
