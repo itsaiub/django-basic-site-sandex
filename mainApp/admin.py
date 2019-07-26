8b8cdd9ecf0e630dd0e5ff93faab3c03bfd51cc6
itsaiub <itsaiub@gmail.com>
2019-07-26 14:26:49 +0600

    added tinymce for better textfield

--- mainApp/admin.py
+++ mainApp/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Tutorial
+from tinymce.widgets import TinyMCE
+from django.db import models
 
 
 class TutorialAdmin(admin.ModelAdmin):
@@ -11,6 +13,10 @@ class TutorialAdmin(admin.ModelAdmin):
         ('Title/date', {'fields': ['tutorial_title', 'tutorial_published']}),
         ('Content', {'fields': ['tutorial_content']})
     ]
+
+    formfield_overrides = {
+        models.TextField: {'widget': TinyMCE()}
+    }
 
 
 # Register your models here.
