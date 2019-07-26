a2fd61c9dd83c2e5c867a1cb577757d6ca620f8b
itsaiub <itsaiub@gmail.com>
2019-07-26 14:18:34 +0600

    add superuser and admin

--- mainApp/admin.py
+++ mainApp/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Tutorial
+
+
+class TutorialAdmin(admin.ModelAdmin):
+    # fields = ['tutorial_title',
+    #           'tutorial_published',
+    #           'tutorial_content']
+
+    fieldsets = [
+        ('Title/date', {'fields': ['tutorial_title', 'tutorial_published']}),
+        ('Content', {'fields': ['tutorial_content']})
+    ]
+
 
 # Register your models here.
+admin.site.register(Tutorial, TutorialAdmin)
