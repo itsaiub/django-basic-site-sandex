b0dd447919cc15237634f5319d6c81a14df14e96
itsaiub <itsaiub@gmail.com>
2019-07-27 00:32:47 +0600

    user registration

--- mainApp/urls.py
+++ mainApp/urls.py
@@ -19,5 +19,6 @@ from . import views
 app_name = 'mainApp'
 
 urlpatterns = [
-    path('', views.homepage, name='homepage')
+    path('', views.homepage, name='homepage'),
+    path('register/', views.register, name='register')
 ]
--- mainApp/views.py
+++ mainApp/views.py
@@ -1,9 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Tutorial
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, logout, authenticate
 
 # Create your views here.
 
 
 def homepage(request):
     return render(request=request, template_name='mainApp/home.html', context={'tutorials': Tutorial.objects.all})
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if(form.is_valid()):
+            user = form.save()
+            login(request, user)
+            return redirect('mainApp:homepage')
+        else:
+            for msg in form.error_messages:
+                print(form.error_messages[msg])
+
+    form = UserCreationForm
+    return render(request, 'mainApp/register.html', context={'form': form})
