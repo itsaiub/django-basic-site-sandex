bf14f48755437549860ccd385e66b22e69742874
itsaiub <itsaiub@gmail.com>
2019-07-28 11:04:31 +0600

    showing messages & log in user

--- mainApp/views.py
+++ mainApp/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from .models import Tutorial
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login, logout, authenticate
-
+from django.contrib import messages
 # Create your views here.
 
 
@@ -14,13 +14,16 @@ def homepage(request):
 def register(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
-        if(form.is_valid()):
+        if form.is_valid():
             user = form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f"New account created: {username}")
             login(request, user)
             return redirect('mainApp:homepage')
         else:
             for msg in form.error_messages:
-                print(form.error_messages[msg])
+                messages.error(request, f"{msg}: {form.error_messages[msg]}")
+            return render(request, 'mainApp/register.html', context={'form': form})
 
     form = UserCreationForm
     return render(request, 'mainApp/register.html', context={'form': form})
