e06bb75a9f673eff5dc9893748ec756f70c54f29
itsaiub <itsaiub@gmail.com>
2019-07-28 23:32:30 +0600

    adding dynamic sidebar

--- mainApp/views.py
+++ mainApp/views.py
@@ -27,7 +27,11 @@ def single_slug(request, single_slug):
     if single_slug in tutorials:
         this_tutorial = Tutorial.objects.get(tutorial_slug=single_slug)
 
-        return render(request, 'mainApp/tutorial.html', {'tutorial': this_tutorial})
+        tutorials_from_series = Tutorial.objects.filter(
+            tutorial_series__tutorial_series=this_tutorial.tutorial_series).order_by('tutorial_published')
+        this_tutorial_idx = list(tutorials_from_series).index(this_tutorial)
+
+        return render(request, 'mainApp/tutorial.html', {'tutorial': this_tutorial, 'sidebar': tutorials_from_series, 'this_tutorial_index': this_tutorial_idx})
 
     return HttpResponse(f'{single_slug} does not correspond to anything')
 
