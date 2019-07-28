5f3f7bc1b8fc5a55e0bc55b9fb53e0eb5967f089
itsaiub <itsaiub@gmail.com>
2019-07-28 12:02:54 +0600

    login and logout implemented

--- mainApp/views.py
+++ mainApp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Tutorial
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 # Create your views here.
@@ -27,3 +27,31 @@ def register(request):
 
     form = UserCreationForm
     return render(request, 'mainApp/register.html', context={'form': form})
+
+
+def logout_request(request):
+    logout(request)
+    messages.info(request, 'Logout successfully')
+    return redirect("mainApp:homepage")
+
+
+def login_request(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.info
+                messages.info(request, f'You are now logged in as {username}')
+                return redirect("mainApp:homepage")
+
+            else:
+                messages.error(request, 'Invalid username or password')
+        else:
+            messages.error(request, 'Invalid username or password')
+
+    form = AuthenticationForm()
+    return render(request, 'mainApp/login.html', context={"form": form})
